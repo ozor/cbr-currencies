a1cf37772d0123fc3a3a85233a9d4fdb5f341955
ozor <ozor@yandex.ru>
2023-10-21 23:59:00 +0500

    Add CbrRates model

---
+++ php-symfony-rabbit/src/Model/CbrRates.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+final readonly class CbrRates
+{
+    public const BASE_CURRENCY_CODE_DEFAULT = 'RUR';
+    public const RATE_DATE_FORMAT = 'd/m/Y';
+    public const CURRENCY_CODE_LENGTH = 3;
+    public const CURRENCY_VALUE_PRECISION = 4;
+}
