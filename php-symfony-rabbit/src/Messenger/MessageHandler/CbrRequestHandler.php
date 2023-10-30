458a52cb6c7a763551134aaa95837f8a53cc7897
ozor <ozor@yandex.ru>
2023-10-30 21:42:22 +0500

    Add logger to CbrRequestHandler

--- php-symfony-rabbit/src/Messenger/MessageHandler/CbrRequestHandler.php
+++ php-symfony-rabbit/src/Messenger/MessageHandler/CbrRequestHandler.php
@@ -2,13 +2,11 @@
 
 namespace App\Messenger\MessageHandler;
 
+use App\Exception\CbrRates\CbrRateNotFoundException;
 use App\Messenger\Message\CbrRatesRequestMessage;
 use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsMessageHandler]
@@ -16,27 +14,30 @@ readonly class CbrRequestHandler
 {
     public function __construct(
         private HttpClientInterface $cbrRatesClient,
+        private LoggerInterface $logger,
     ) {
     }
 
-    /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
-     * @throws Exception
-     */
     public function __invoke(CbrRatesRequestMessage $message): string
     {
-        // Set random delay for avoid spam ban from CBR
-        usleep(random_int(100000, 500000));
+        try {
+            // Set random delay for avoid spam ban from CBR
+            usleep(random_int(100000, 500000));
 
-        return $this->cbrRatesClient->request(
-            $message->method,
-            $message->url,
-            [
-                'query' => $message->query,
-            ]
-        )->getContent();
+            return $this->cbrRatesClient->request(
+                $message->method,
+                $message->url,
+                [
+                    'query' => $message->query,
+                ]
+            )->getContent();
+        } catch (Exception $exception) {
+            $this->logger->error($exception->getMessage(), [
+                'exception' => $exception,
+                'method' => __METHOD__,
+                'message' => $message,
+            ]);
+            throw new CbrRateNotFoundException();
+        }
     }
 }
